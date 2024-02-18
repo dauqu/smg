185c57d643e3befcd0c5c13d7d0477b3df0288a0
Harsh Singh <sujeetsingh734321@gmail.com>
2024-02-18 18:53:37 +0530

    chnages

--- src/app/components/header.jsx
+++ src/app/components/header.jsx
@@ -5,7 +5,25 @@ export default function Header(params) {
         <img className="w-10 h-10" src="/logo.png" />
         <a className="btn btn-ghost no-animation text-xl">SMG Group</a>
       </div>
-      <div className="flex-none gap-2">
+      {/* Menu ICON */}
+      <button className="btn btn-sm btn-neutral flex sm:hidden">
+        Login
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="16"
+          height="16"
+          fill="currentColor"
+          class="bi bi-arrow-right"
+          viewBox="0 0 16 16"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8"
+          />
+        </svg>
+      </button>
+      <div></div>
+      <div className="flex-none gap-2 hidden sm:flex">
         {/* Right Side Panel */}
         <label className="input input-bordered flex input-sm items-center gap-2">
           <svg
