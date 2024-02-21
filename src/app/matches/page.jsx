f832ccf724029cf1b8aa246fbec01dd82b266275
Harsh Singh <sujeetsingh734321@gmail.com>
2024-02-21 21:56:23 +0530

    chnages

--- src/app/matches/page.jsx
+++ src/app/matches/page.jsx
@@ -12,15 +12,20 @@ export default function Page(params) {
   const [loading, setLoading] = React.useState(true);
   const [selected, setSelected] = React.useState([]);
 
-//   const searchParams = useSearchParams();
+  //   const searchParams = useSearchParams();
 
-  const queryString = window.location.search;
-  const urlParams = new URLSearchParams(queryString);
+  let queryString = "";
+  let urlParams = null;
 
-  const event_type_id = urlParams.get('eventtypeid')
-  const competition_id = urlParams.get('competitionid')
-//   const event_type_id = searchParams.get("eventtypeid");
-//   const competition_id = searchParams.get("competitionid");
+  if (typeof window !== "undefined") {
+    queryString = window.location.search;
+    urlParams = new URLSearchParams(queryString);
+  }
+
+  const event_type_id = urlParams ? urlParams.get("eventtypeid") : null;
+  const competition_id = urlParams ? urlParams.get("competitionid") : null;
+  //   const event_type_id = searchParams.get("eventtypeid");
+  //   const competition_id = searchParams.get("competitionid");
 
   async function getMatches() {
     setLoading(true);
