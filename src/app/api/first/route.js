5dc23c566b2b05eee04031c20befc25ee7172aa0
Harsh Singh <sujeetsingh734321@gmail.com>
2024-02-21 20:30:24 +0530

    updated code

--- src/app/api/first/route.js
+++ src/app/api/first/route.js
@@ -5,10 +5,23 @@ import { NextResponse } from "next/server";
 export async function GET(request) {
   try {
     // Make a GET request to https://api.daucu.site
-    const response = await axios.get("http://142.93.36.1/api/v1/fetch_data?Action=listEventTypes");
+    const response = await axios.get(
+      "http://142.93.36.1/api/v1/fetch_data?Action=listEventTypes"
+    );
+
+    const headers = {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET,POST,PUT,DELETE,OPTIONS",
+      "Access-Control-Allow-Headers":
+        "Content-Type, Access-Control-Allow-Headers",
+      "Access-Control-Allow-Credentials": true,
+    };
 
     // Return the response data to the client
-    return NextResponse.json(response.data, { status: response.status });
+    return NextResponse.json(response.data, {
+      status: response.status,
+      headers,
+    });
   } catch (error) {
     // Handle any errors
     console.error("Error making GET request:", error.message);
--- src/app/cricket/page.jsx
+++ src/app/cricket/page.jsx
@@ -1,15 +1,62 @@
+"use client";
+
+import * as React from "react";
+
+import axios from "axios";
 import Header from "../components/header";
 import SubHeader from "../components/sub-header";
 
 export default function Page(params) {
+  const [data, setUsers] = React.useState([]);
+  const [loading, setLoading] = React.useState(true);
+
+  async function mySitesFunction() {
+    setLoading(true);
+    await axios
+      .get(`http://65.20.66.239:3000/api/first`, {})
+      .then((res) => {
+        setLoading(false);
+        console.log(res.data);
+        setUsers(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+        setLoading(false);
+      });
+  }
+
+  React.useEffect(() => {
+    mySitesFunction();
+  }, []);
   return (
     <div>
       <div className="fixed top-0 left-0 right-0 z-10">
         <Header />
         <SubHeader />
       </div>
-      <div>
-        Harsha Web 
+      <div className="mt-36">
+        {/* Tabs */}
+        {loading ? (
+          "Loading..."
+        ) : (
+          <div role="tablist" className="tabs tabs-bordered">
+            {data &&
+              data.map((item) => (
+                <div>
+                  <input
+                    type="radio"
+                    name="my_tabs_1"
+                    role="tab"
+                    className="tab"
+                    aria-label="Tab 1"
+                  />
+                  <div role="tabpanel" className="tab-content p-10">
+                    {item?.name ?? "Loading..."}
+                  </div>
+                </div>
+              ))}
+          </div>
+        )}
       </div>
     </div>
   );
