7ebd5eea7ded03621a2b7c1c3e91fcc49c71c1a2
Harsh Singh <sujeetsingh734321@gmail.com>
2024-02-21 23:52:27 +0530

    chnages

--- src/app/api/markets-runner/[id]/page.js
+++ src/app/api/markets-runner/[id]/page.js
@@ -4,32 +4,30 @@ import axios from "axios";
 // To handle a GET request to /api
 export async function GET(request, { params }) {
   const param = params.id;
-  try {
-    // Make a GET request to https://api.daucu.site
-    const response = await axios.get(
-      `http://142.93.36.1/api/v1/fetch_data?Action=listMarketRunner&MarketID=${param}`,
-      { responseType: "stream" } // Stream the response
-    );
 
-    const headers = {
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Methods": "GET,POST,PUT,DELETE,OPTIONS",
-      "Access-Control-Allow-Headers":
-        "Content-Type, Access-Control-Allow-Headers",
-      "Access-Control-Allow-Credentials": true,
-    };
+  // Make a GET request to https://api.daucu.site
+  return axios.get(`http://142.93.36.1/api/v1/fetch_data?Action=listMarketRunner&MarketID=${param}`)
+    .then((response) => {
+      const headers = {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET,POST,PUT,DELETE,OPTIONS",
+        "Access-Control-Allow-Headers":
+          "Content-Type, Access-Control-Allow-Headers",
+        "Access-Control-Allow-Credentials": true,
+      };
 
-    // Stream the response data to the client
-    return NextResponse.stream(response.data, {
-      status: response.status,
-      headers,
+      // Return the response data to the client
+      return NextResponse.json(response.data, {
+        status: response.status,
+        headers,
+      });
+    })
+    .catch((error) => {
+      // Handle any errors
+      console.error("Error making GET request:", error.message);
+      return NextResponse.json(
+        { error: "Error making GET request" },
+        { status: 500 }
+      );
     });
-  } catch (error) {
-    // Handle any errors
-    console.error("Error making GET request:", error.message);
-    return NextResponse.json(
-      { error: "Error making GET request" },
-      { status: 500 }
-    );
-  }
 }
