91a2d7277cbf70039ecbb2a8e45072392d3ac5b2
Harsh Singh <sujeetsingh734321@gmail.com>
2024-02-22 10:20:38 +0530

    dfgdfgfd

--- src/app/api/market-odds/[id]/route.js
+++ src/app/api/market-odds/[id]/route.js
@@ -1,12 +1,12 @@
-import axios from "axios";
 import { NextResponse } from "next/server";
+import axios from "axios";
 
 // To handle a POST request to /api
-export function GET(request, { params }, res) {
+export async function GET(request, { params }) {
   const param = params.id;
 
   // Make a GET request to https://api.daucu.site
-  axios.get(`http://142.93.36.1/api/v1/listMarketBookOdds?market_id=${param}`)
+  return axios.get(`http://142.93.36.1/api/v1/listMarketBookOdds?market_id=${param}`)
     .then(response => {
       const headers = {
         "Access-Control-Allow-Origin": "*",
@@ -17,17 +17,18 @@ export function GET(request, { params }, res) {
         "Accept-Encoding": "gzip, deflate",
       };
 
-      // Set the headers
-      Object.keys(headers).forEach((key) => {
-        res.setHeader(key, headers[key]);
-      });
-
       // Return the response data to the client
-      res.status(response.status).json(response.data);
+      return NextResponse.json(response.data, {
+        status: response.status,
+        headers,
+      });
     })
     .catch(error => {
       // Handle any errors
       console.error("Error making GET request:", error.message);
-      res.status(500).json({ error: "Error making GET request" });
+      return NextResponse.json(
+        { error: "Error making GET request" },
+        { status: 500 }
+      );
     });
 }
