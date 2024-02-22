589fe8cafc8390f245b0adf04ca14cb1a0ca2007
Harsh Singh <sujeetsingh734321@gmail.com>
2024-02-22 10:19:43 +0530

    dfdhfdf

--- src/app/api/market-odds/[id]/route.js
+++ src/app/api/market-odds/[id]/route.js
@@ -1,35 +1,33 @@
-import { NextResponse } from "next/server";
 import axios from "axios";
+import { NextResponse } from "next/server";
 
 // To handle a POST request to /api
-export async function GET(request, { params }, res) {
+export function GET(request, { params }, res) {
   const param = params.id;
-  try {
-    // Make a GET request to https://api.daucu.site
-    const response = await axios.get(
-      `http://142.93.36.1/api/v1/listMarketBookOdds?market_id=${param}`
-    );
 
-    // const headers = {
-    //   "Access-Control-Allow-Origin": "*",
-    //   "Access-Control-Allow-Methods": "GET,POST,PUT,DELETE,OPTIONS",
-    //   "Access-Control-Allow-Headers":
-    //     "Content-Type, Access-Control-Allow-Headers",
-    //   "Access-Control-Allow-Credentials": true,
-    //   "Accept-Encoding": "gzip, deflate",
-    // };
+  // Make a GET request to https://api.daucu.site
+  axios.get(`http://142.93.36.1/api/v1/listMarketBookOdds?market_id=${param}`)
+    .then(response => {
+      const headers = {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET,POST,PUT,DELETE,OPTIONS",
+        "Access-Control-Allow-Headers":
+          "Content-Type, Access-Control-Allow-Headers",
+        "Access-Control-Allow-Credentials": true,
+        "Accept-Encoding": "gzip, deflate",
+      };
 
-    // Return the response data to the client
-    return NextResponse.json(response.data, {
-      status: response.status,
-      // headers,
+      // Set the headers
+      Object.keys(headers).forEach((key) => {
+        res.setHeader(key, headers[key]);
+      });
+
+      // Return the response data to the client
+      res.status(response.status).json(response.data);
+    })
+    .catch(error => {
+      // Handle any errors
+      console.error("Error making GET request:", error.message);
+      res.status(500).json({ error: "Error making GET request" });
     });
-  } catch (error) {
-    // Handle any errors
-    console.error("Error making GET request:", error.message);
-    return NextResponse.json(
-      { error: "Error making GET request" },
-      { status: 500 }
-    );
-  }
 }
