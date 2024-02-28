f918a70a72a2ead68ecbcbfa5641b8c4983d6efd
Harsh Singh <sujeetsingh734321@gmail.com>
2024-02-28 19:30:35 +0530

    URL chnages

--- src/app/cricket/page.jsx
+++ src/app/cricket/page.jsx
@@ -19,7 +19,7 @@ export default function Page(params) {
   async function mySitesFunction() {
     setLoading(true);
     await axios
-      .get(`http://localhost:3000/api/first`, {})
+      .get(`https://onlinebookbazar.com/api/first`, {})
       .then((res) => {
         setLoading(false);
         console.log(res.data);
@@ -38,7 +38,7 @@ export default function Page(params) {
     setLoadingE(true);
     await axios
       .get(
-        `http://localhost:3000/api/series-by-sport/${selected.eventType}`,
+        `https://onlinebookbazar.com/api/series-by-sport/${selected.eventType}`,
         {}
       )
       .then((res) => {
@@ -58,7 +58,7 @@ export default function Page(params) {
     setGettingDetails(true);
     try {
       const res = await axios.get(
-        `http://localhost:3000/api/matches/${event_type_id}/${competition_id}`
+        `https://onlinebookbazar.com/api/matches/${event_type_id}/${competition_id}`
       );
       setGettingDetails(false);
       setDetailsData(res?.data ?? []);
